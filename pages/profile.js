e5fd361fa94decff55ee30ccdb303a82f3d21081
J4-CK <142830650+J4-CK@users.noreply.github.com>
2025-01-09 23:54:58 -0600

    debug commit

--- pages/profile.js
+++ pages/profile.js
@@ -7,6 +7,7 @@ export default function ProfilePage() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  
   // Fetch session on load
   useEffect(() => {
     async function fetchUserData() {
