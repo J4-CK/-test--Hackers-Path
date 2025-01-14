05944b3efada2d5886532fc9c5e3275de62180b2
TRXe8 <169096534+TRXe8@users.noreply.github.com>
2025-01-13 23:32:01 -0600

    Update risk-basics-presentation.js
    Changing to a one-page lesson

--- pages/lessons/risk-basics-presentation.js
+++ pages/lessons/risk-basics-presentation.js
@@ -9,41 +9,27 @@ export default function RiskBasicsLesson() {
       title: "What is Risk?",
       content: (
         <>
+          <h2>What is Risk?</h2>
           <p>Risk is simply the possibility of something bad happening. Every decision you make has risk involved. </p>
           <p>For example, think about riding a bike. You can move much faster, travel farther, and sometimes use the road. However, the chance of injury is higher compared to walking.</p>
           <p><b>Risk tolerance</b> is the amount of risk someone is willing to take. Each person has a different level of risk tolerance.</p>
-        </>
-      ),
-    },
-    {
-      title: "Risk Terminology",
-      content: (
-        <>
+
+          <h2>Risk Terminology</h2>
           <p>Here are some terms that are related to risk:</p>
           <ul>
             <li><b>Asset:</b> Something you want to protect. For example, your phone would be considered an asset.</li>
             <li><b>Vulnerability:</b> A weakness in protection. An example of a vulnerability is a weak password for your phone.</li>
             <li><b>Threat:</b> Someone or something trying to exploit a vulnerability. For example, if someone knows you have a weak password, they could try to access your phone. This person or action is the threat.</li>
           </ul>
-        </>
-      ),
-    },
-    {
-      title: "Risk Assessment",
-      content: (
-        <>
+
+          <h2>Risk Assessment</h2>
           <p>There are two main factors when it comes to calculating risk:</p>
           <ol>
             <li>How severe the bad outcome is.</li>
             <li>How likely it is that something bad will happen.</li>
           </ol>
-        </>
-      ),
-    },
-    {
-      title: "Risk Assessment Example",
-      content: (
-        <>
+
+          <h3>Risk Assessment Example</h3>
           <p>Imagine you are not doing well in a class and are considering cheating on a test. This could help you get a high score to boost your grade. However, the severity of the risk is that if you are caught, your test score becomes a zero and you might face punishment from the school. The likelihood of the risk can change, but let's assume your teacher is known for catching cheaters.</p>
           <p>Let’s assess this risk:</p>
           <ul>
