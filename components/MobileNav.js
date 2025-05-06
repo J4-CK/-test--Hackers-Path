f9d0d2b114b442401ef6a4f51f89e605213d7042
MarcusProgramming <126976032+MarcusProgramming@users.noreply.github.com>
2025-05-05 23:05:04 -0500

    Revert "Update MobileNav.js"
    This reverts commit 1a41c5119945081a3e2a3488ba3bc8264a9db228.

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -160,11 +160,6 @@ export default function MobileNav({ username }) {
               </Link>
             </li>
           </ul>
-            <li className={currentPath === '/leaderboard' ? 'active' : ''}>
-              <Link href="/leaderboard">
-                <span>Leaderboard</span>
-              </link>
-            
           
           <div className="menu-footer">
             <form action="/api/auth/logout" method="POST">
